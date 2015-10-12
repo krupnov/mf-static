8de100a177d844a1b7fc7f57b032706b9dc5cb47
Pogoss <j.pogosyan@velidit.ru>
2015-10-12 23:27:45 +0300

    add offers block to get credit page, add show hide effect in credit status page

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,4 +1,6 @@
 $(document).on('ready page:load', function() {
+	creditBlocks.init();
+		
 	$('#mobile-nav').sidr({
 		displace: false,
 		onOpen: function(name) {
@@ -53,5 +55,35 @@ $(document).on('ready page:load', function() {
 	$('#slider-time').noUiSlider(time_options);
 	$('#slider-time').Link('lower').to($('#time-count'));
 	
-	$('.chosen-select').chosen({disable_search: true});
+	// $('.chosen-select').chosen({disable_search: true});
 });
+
+var creditBlocks = {
+	init: function() {
+		if($('div.credit-status-block').length > 0) {
+			var showText = '> Показать';
+			var hideText = 'v Скрыть';
+			
+			var is_visible = false;
+			
+			// hide all of the elements with a class of 'toggle'
+			$('#flip-block').hide();
+			
+			// capture clicks on the toggle links
+			$('#flip').click(function() {
+			
+				// switch visibility
+				is_visible = !is_visible;
+			
+				// change the link depending on whether the element is shown or hidden
+				$(this).html( (!is_visible) ? showText : hideText);
+				
+				$(this).parent().next('#flip-block').toggle('slow');
+			
+			// return false so any link destination is not followed
+				return false;
+			});
+
+		}
+	}
+}
