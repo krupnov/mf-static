c523d62ab0ec4ad466ce8c57e40e8ac1ad5af511
unknown <krupnov@krupnov.avp.ru>
2015-10-15 13:42:24 +0300

    Misprint fix

--- javascripts/application.js
+++ javascripts/application.js
@@ -116,7 +116,7 @@ var getCreditSliders = {
 				input_time.value = values[handle];
 			});
 			
-			input_ешьу.addEventListener('change', function(){
+			input_time.addEventListener('change', function(){ 
 				slider_time.noUiSlider.set(this.value);
 			});
 		}
