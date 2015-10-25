0816bd41a9c76ec2c9f2a9a4d9be9d137b600021
Ilya <krupnov.iv@gmail.com>
2015-10-25 15:25:38 +0300

    LOan state update implemented

--- javascripts/currentloan.js
+++ javascripts/currentloan.js
@@ -5,7 +5,25 @@ $(document).ready(function() {
 	
 	var localStartDate = moment(loanStartDate).toDate();
 	var localEndDate = moment(loanEndDate).toDate();
+	var loanStateSpan = document.getElementById("loan-state");
+	var refundButton = $("#refund");
 	
 	$("#loan-period").text(moment(localStartDate).format(messages["date.pattern"]) + " - "
 			+ moment(localEndDate).format(messages["date.pattern"]));
+	setInterval(function() {
+		$.ajax({
+			type: "GET",
+			url: loanStateUrl,
+			contentType: "application/json;charset=UTF-8",
+			dataType: "json",
+			success: function(data, textStatus) {
+				loanStateSpan.textContent = data.state;
+				if (data.canRefund) {
+					refundButton.show("slow");
+				} else {
+					refundButton.hide("slow");
+				}
+			}
+		});
+	}, 10000)
 });
