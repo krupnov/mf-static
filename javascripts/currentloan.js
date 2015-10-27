c59147e906770a15bf74bec2948ed018b80be426
Ilya <krupnov.iv@gmail.com>
2015-10-27 10:01:50 +0300

    Loan details implemented

--- javascripts/currentloan.js
+++ javascripts/currentloan.js
@@ -9,29 +9,33 @@ $(document).ready(function() {
 	var refundButton = $("#refund");
 	var initialAmount = document.getElementById("initial-amount");
 	initialAmount.textContent = numberWithSpaces(initialAmount.textContent);
-	var debt = document.getElementById("debt");
-	debt.textContent = numberWithSpaces(debt.textContent);
+	
 	
 	$("#loan-period").text(moment(localStartDate).format(messages["date.pattern"]) + " - "
 			+ moment(localEndDate).format(messages["date.pattern"]));
-	setInterval(function() {
-		$.ajax({
-			type: "GET",
-			url: loanStateUrl,
-			contentType: "application/json;charset=UTF-8",
-			dataType: "json",
-			success: function(data, textStatus) {
-				loanStateSpan.textContent = data.state;
-				if (data.canRefund) {
-					refundButton.show("slow");
-				} else {
-					refundButton.hide("slow");
+	if (currentLoan) {
+		var debt = document.getElementById("debt");
+		debt.textContent = numberWithSpaces(debt.textContent);
+		
+		setInterval(function() {
+			$.ajax({
+				type: "GET",
+				url: loanStateUrl,
+				contentType: "application/json;charset=UTF-8",
+				dataType: "json",
+				success: function(data, textStatus) {
+					loanStateSpan.textContent = data.state;
+					if (data.canRefund) {
+						refundButton.show("slow");
+					} else {
+						refundButton.hide("slow");
+					}
+					debt.textContent = numberWithSpaces(data.debt);
+				},
+				error: function() {
+					location.reload(true);
 				}
-				debt.textContent = numberWithSpaces(data.debt);
-			},
-			error: function() {
-				location.reload(true);
-			}
-		});
-	}, 10000)
+			});
+		}, 10000);
+	}
 });
