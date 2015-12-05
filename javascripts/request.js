103d784628edb4b02021b1c167d402085336b490
Ilya Krupnov <krupnov.iv@gmail.com>
2015-12-05 23:42:34 +0300

    Validation added

--- javascripts/request.js
+++ javascripts/request.js
@@ -4,6 +4,26 @@
 
 $(document).ready(function() {
 	chooseCreditTransferOption.init();
+	
+	document.getElementById('yandex-button').addEventListener("click", function () {
+		if ($("#yandex-form").valid())
+			$("#yandex-form").submit();
+	});
+	
+	jQuery.extend(jQuery.validator.messages, {
+		required: messages["org.hibernate.validator.constraints.NotEmpty.message"],
+		number: messages["typeMismatch.java.lang.Integer"]
+	});
+	$("#yandex-form").validate({
+		rules: {
+			yandexAccount: {
+				required: true
+			}
+		},
+		errorPlacement: function(error, element) {
+				error.insertAfter(element);
+			}
+		});
 });
 
 var chooseCreditTransferOption = {
